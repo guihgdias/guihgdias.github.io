1c82b96fff700f28e7e0a61e433aa80052d2863a
Guilherme Henrique Galdino Dias <guilhermehgdiasbr@gmail.com>
2022-12-21 23:10:05 -0300

    Fix images missing

--- scripts/list.js
+++ scripts/list.js
@@ -4,39 +4,39 @@ let infosJson = [
       {
         id: 1,
         title: 'HTML',
-        image: '../assets/images/knowledges/html.svg'
+        image: 'assets/images/knowledges/html.svg'
       },
       {
         id: 2,
         title: 'CSS',
-        image: '../assets/images/knowledges/css.svg'
+        image: 'assets/images/knowledges/css.svg'
       },
       {
         id: 3,
         title: 'JavaScript',
-        image: '../assets/images/knowledges/js.svg'
+        image: 'assets/images/knowledges/js.svg'
       },
       {
         id: 4,
         title: 'GIT',
-        image: '../assets/images/knowledges/git.svg'
+        image: 'assets/images/knowledges/git.svg'
       }
     ],
     tools: [
       {
         id: 1,
         title: 'Visual Studio Code',
-        image: '../assets/images/knowledges/vscode.svg'
+        image: 'assets/images/knowledges/vscode.svg'
       },
       {
         id: 2,
         title: 'Figma',
-        image: '../assets/images/knowledges/figma.svg'
+        image: 'assets/images/knowledges/figma.svg'
       },
       {
         id: 3,
         title: 'GitHub',
-        image: '../assets/images/knowledges/github.svg'
+        image: 'assets/images/knowledges/github.svg'
       }
     ],
     projects: [
@@ -44,21 +44,21 @@ let infosJson = [
         id: 1,
         title: 'Drumpads',
         description: 'Drumpads feito em JS.',
-        tecsUsed: 'HTML, CSS e JS Vanilla', image: '../assets/images/projects/drumpads.png',
+        tecsUsed: 'HTML, CSS e JS Vanilla', image: 'assets/images/projects/drumpads.png',
         url: 'https://guihgdias.github.io/drumpads/'
       },
       {
         id: 2,
         title: 'Christmas E-commerce',
         description: 'Desige de uma loja virtual com tema de natal.',
-        tecsUsed: 'HTML, CSS e JS Vanilla', image: '../assets/images/projects/christmas-ecommerce-design.png',
+        tecsUsed: 'HTML, CSS e JS Vanilla', image: 'assets/images/projects/christmas-ecommerce-design.png',
         url: 'https://guihgdias.github.io/christmas-website-design/'
       },
       {
         id: 3,
         title: 'Facilita Tasks',
         description: 'Todo List simples com persistência de dados em localstorage.',
-        tecsUsed: 'HTML, CSS e JS Vanilla', image: '../assets/images/projects/facilita-task.png',
+        tecsUsed: 'HTML, CSS e JS Vanilla', image: 'assets/images/projects/facilita-task.png',
         url: 'https://guihgdias.github.io/facilitatask-teste/'
       }
     ]
